39807e08456dfe7f68ac204f5e7d1af8db1eb0b3
collenirwin <collenirwin@gmail.com>
2020-05-22 03:28:43 -0400

    add AstPrinter visitor

---
+++ Lang/Interpreter/AstPrinter.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+
+namespace Lang.Interpreter
+{
+    /// <summary>
+    /// Creates a string representation of AST nodes.
+    /// </summary>
+    internal class AstPrinter : IExpressionVisitor<string>
+    {
+        /// <summary>
+        /// Creates a string representation of AST nodes.
+        /// </summary>
+        /// <param name="expression">Expression to print.</param>
+        /// <returns>A string representation of AST nodes in the expression.</returns>
+        public string Print(ExpressionBase expression)
+        {
+            return expression.Accept(this);
+        }
+
+        public string VisitBinaryExpression(BinaryExpression expression)
+        {
+            return Parenthesize(expression.Operator.WrappedSource, expression.LeftOperand, expression.RightOperand);
+        }
+
+        public string VisitGroupingExpression(GroupingExpression expression)
+        {
+            return Parenthesize("group", expression.Expression);
+        }
+
+        public string VisitLiteralExpression(LiteralExpression expression)
+        {
+            return expression.Value?.ToString() ?? "null";
+        }
+
+        public string VisitUnaryExpression(UnaryExpression expression)
+        {
+            return Parenthesize(expression.Operator.WrappedSource, expression.Operand);
+        }
+
+        private string Parenthesize(string name, params ExpressionBase[] expressions) =>
+            $"({name} {string.Join(" ", expressions.Select(expression => expression.Accept(this)))})";
+    }
+}
