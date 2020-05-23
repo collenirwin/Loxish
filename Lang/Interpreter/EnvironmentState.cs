a7244702b9111849e92bbaae74adb0de5fc3b55c
collenirwin <collenirwin@gmail.com>
2020-05-23 13:01:24 -0400

    add EnvironmentState class to allow variable storage/access

---
+++ Lang/Interpreter/EnvironmentState.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace Lang.Interpreter
+{
+    /// <summary>
+    /// Keeps variable state.
+    /// </summary>
+    public class EnvironmentState
+    {
+        private readonly Dictionary<string, object> _values = new Dictionary<string, object>();
+
+        /// <summary>
+        /// Add a variable to the internal state dictionary.
+        /// </summary>
+        /// <param name="name">Name of the variable.</param>
+        /// <param name="value">Value of the variable.</param>
+        public void Define(string name, object value)
+        {
+            _values.Add(name, value);
+        }
+
+        /// <summary>
+        /// Gets the value the corresponds with the name token.
+        /// </summary>
+        /// <param name="name">The variable name token.</param>
+        /// <exception cref="RuntimeException"/>
+        /// <returns>The value the corresponds with the name token.</returns>
+        public object GetValue(Token name)
+        {
+            if (_values.ContainsKey(name.WrappedSource))
+            {
+                return _values[name.WrappedSource];
+            }
+
+            throw new RuntimeException(name, $"'{name.WrappedSource}' is undefined.");
+        }
+    }
+}
