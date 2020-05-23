56cba33d1f2243d1f8b1ebe429f8fdd6ae0478de
collenirwin <collenirwin@gmail.com>
2020-05-23 11:41:54 -0400

    Add some statement classes, IStatementVisitor interface

---
+++ Lang/Interpreter/IStatementVisitor.cs
@@ -0,0 +1,12 @@
+﻿namespace Lang.Interpreter
+{
+    /// <summary>
+    /// Definines a visitor contract for each <see cref="StatementBase"/> type.
+    /// </summary>
+    /// <typeparam name="T">Type resulting from each Visit method.</typeparam>
+    public interface IStatementVisitor<T>
+    {
+        T VisitExpressionStatement(ExpressionStatement statement);
+        T VisitPrintStatement(PrintStatement statement);
+    }
+}
---
+++ Lang/Interpreter/Statements.cs
@@ -0,0 +1,29 @@
+﻿namespace Lang.Interpreter
+{
+    /// <summary>
+    /// Base class for all statements.
+    /// </summary>
+    public abstract class StatementBase
+    {
+    }
+
+    public class ExpressionStatement : StatementBase
+    {
+        public ExpressionBase Expression { get; }
+
+        public ExpressionStatement(ExpressionBase expression)
+        {
+            Expression = expression;
+        }
+    }
+
+    public class PrintStatement : StatementBase
+    {
+        public ExpressionBase Expression { get; }
+
+        public PrintStatement(ExpressionBase expression)
+        {
+            Expression = expression;
+        }
+    }
+}
